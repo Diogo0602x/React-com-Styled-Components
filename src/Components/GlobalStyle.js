88da9409d9014190dcb70a880f7fb8586011ae20
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-06-14 16:52:26 -0300

    Aula 5 terminada e curso finalizado

--- src/Components/GlobalStyle.js
+++ src/Components/GlobalStyle.js
@@ -1,12 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-* {
-  box-sizing: border-box;
-  font-family: "Montserrat", sans-serif;
-  margin: 0;
-  padding: 0;
-  text-decoration: none;
-  color: grey;
-}
+  * {
+    box-sizing: border-box;
+    font-family: "Montserrat", sans-serif;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+  }
 `;
